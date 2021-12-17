aaba450a7344590edebe94222e687d8525501a26
luzianegering <lu.gering@gmail.com>
2021-12-16 22:53:50 -0300

    Ansiedade

--- jquery.js
+++ jquery.js
@@ -1,11 +1,10 @@
 //Essa página utiliza JQuery.
 function nomefunction(){
   const necessario= $("button");
-necessario.on("click", function(event){
+necessario.on("click", function(){
   $(this).data('clicked', true);
   if(necessario.data('clicked')) {
   const data= $("#istrue").val();
-  alert(data);
  const url= "https://api.nasa.gov/planetary/apod?api_key=QhO49Q7ImWWJHhb4ycrhKP6eohheNhe4vBJfP58J&date="+data;
  $.ajax({
    url: url,
@@ -23,21 +22,6 @@ necessario.on("click", function(event){
             }
 });}
 });}
-
-function fraseAleatoria() {
-  $.getJSON( "https://api.nasa.gov/planetary/apod?api_key=QhO49Q7ImWWJHhb4ycrhKP6eohheNhe4vBJfP58J", function( data ) {
-    const items = [];
-    $.each( data, function( chave, item ) {
-      items.push( "<p id='text" + chave + "'>" + item + "</p>" );
-    });
-   
-    $( "<main/>", {
-      "class": "conteudo-principal",
-      html: items.join( "" )
-    }).appendTo( "body" );
-  });
-
-}
 
 $(function(){
   nomefunction()
