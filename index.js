ea245e9ba5d4f522f40ff930251c8cada784cdc6
Luzi <lu.gering@gmail.com>
2022-05-18 23:29:42 -0300

    Renomeadas as páginas

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-//Essa página utiliza JQuery.
 function nomefunction(){
   const necessario= $("button");
+  const caixaPrincipal = $("doisel")
 necessario.on("click", function(){
   $(this).data('clicked', true);
   if(necessario.data('clicked')) {
@@ -11,18 +11,27 @@ necessario.on("click", function(){
     type: "get",
       dataType: "json",
         success:function(datos){
-          const end= (datos.url);
+          const end= (datos.hdurl);
           const imagem= $(".imagem").attr("src",end);
           const divp= $("#conteudo-recebido")
           const descricao= (datos.explanation);
-       const texto= $("#caixinha").text(descricao);
-       imagem.append(imagem);
-            texto.append(texto);
-            divp.addClass("bela-bordatxt");
+          const texto= $("#caixinha").text(descricao);
+           caixaPrincipal.html(imagem);
             }
+             
+});}    
 });}
-});}
-
+function imagemDia(){
+  $.ajax({
+    url:`https://api.nasa.gov/planetary/apod?api_key=If038K0asBSljpY2HtQWwy0fefuPkqSBsxCpeKqT`,
+    type: 'GET',
+    success: function(fotoDia){
+        console.log(fotoDia)
+        $('#caixinha').text(fotoDia.explanation)
+        $('.imagem').attr('src', fotoDia.url)
+       
+    }});}
 $(function(){
+  imagemDia()
   nomefunction()
 });
